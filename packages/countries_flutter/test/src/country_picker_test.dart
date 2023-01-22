32f67a0756a7ccd82d7ebc6c4480cec92745a4c1
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2023-01-22 21:47:09 +0100

    feat(ui): added basic country picker widget body

--- packages/countries_flutter/test/src/country_picker_test.dart
+++ packages/countries_flutter/test/src/country_picker_test.dart
@@ -1,5 +1,9 @@
+CountryPickerimport "package:countries_flutter/src/country_picker.dart";
 import "package:flutter_test/flutter_test.dart";
 
-void main() => group("CountryPicker", () {
-      test("test", () => expect(true, isTrue));
+void main() => group("$CountryPicker", () {
+      test(
+        "parameters",
+        () => expect(const CountryPicker().countries.isEmpty, isTrue),
+      );
     });
