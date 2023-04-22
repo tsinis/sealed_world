eb33a57ff9d71ed0e795836aa9f7f23bd4436d1c
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2023-04-22 20:45:05 +0200

    refactor: refactoring country helpers

--- packages/sealed_countries/lib/src/helpers/extensions/world_country/geo_extension.dart
+++ packages/sealed_countries/lib/src/helpers/extensions/world_country/geo_extension.dart
@@ -2,17 +2,17 @@ import "../../../../sealed_countries.dart";
 import "../list_double_extension.dart";
 
 extension GeoExtension on WorldCountry {
-  static const imperialMultiplier = 0.38610216;
+  static const imperialAreaMultiplier = 0.38610216;
 
   ({double latitude, double longitude})? get latitudeLongitude =>
       latLng.latitudeLongitude;
 
   List<WorldCountry>? get borders {
     final codes = bordersCodes;
-    if (codes == null) return null;
+    if (codes == null || codes.isEmpty) return null;
 
     return List.unmodifiable(codes.map<WorldCountry>(WorldCountry.fromCode));
   }
 
-  double get areaImperial => areaMetric * imperialMultiplier;
+  double get areaImperial => areaMetric * imperialAreaMultiplier;
 }
