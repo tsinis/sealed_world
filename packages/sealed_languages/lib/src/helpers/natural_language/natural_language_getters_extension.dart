bf6aa16f977f8ad3f12a88b6c40c316b69790854
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2023-05-14 16:01:32 +0200

    feat: added natural language getters extension with iso getters

---
+++ packages/sealed_languages/lib/src/helpers/natural_language/natural_language_getters_extension.dart
@@ -0,0 +1,21 @@
+import "../../model/language/language.dart";
+
+/// Provides convenient getters for ISO 639 language codes
+/// on a [NaturalLanguage] class.
+extension NaturalLanguageGettersExtension on NaturalLanguage {
+  /// Returns the ISO 639-1 language code for a [NaturalLanguage].
+  ///
+  /// Provides same data as [codeShort].
+  String get iso639one => codeShort;
+
+  /// Returns the ISO 639-2/T language code for this [NaturalLanguage].
+  ///
+  /// Provides same data as [code].
+  String get iso639twoT => code;
+
+  /// Returns the ISO 639-2/B bibliographic language code for this [NaturalLanguage],
+  /// or `null` if none is available.
+  ///
+  /// Provides same data as [bibliographicCode].
+  String? get iso639twoB => bibliographicCode;
+}
---
+++ packages/sealed_languages/test/src/helpers/natural_language/natural_language_getters_extension_test.dart
@@ -0,0 +1,17 @@
+import "package:sealed_languages/src/helpers/natural_language/natural_language_getters_extension.dart";
+import "package:sealed_languages/src/model/language/language.dart";
+import "package:test/test.dart";
+
+void main() => group("NaturalLanguageGettersExtension", () {
+      final value = NaturalLanguage.list
+          .firstWhere((language) => language.bibliographicCode != null);
+
+      test("iso639one", () => expect(value.iso639one, value.codeShort));
+
+      test("iso639twoT", () => expect(value.iso639twoT, value.code));
+
+      test(
+        "iso639twoB",
+        () => expect(value.iso639twoB, value.bibliographicCode),
+      );
+    });
