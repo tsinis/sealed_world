0b6116c14e6e07e25d2b0718017206ba8d19f6db
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2023-07-13 16:31:12 +0200

    feat(extension): added extensions for langs and countries

---
+++ packages/world_countries/lib/src/extensions/natural_language_extension.dart
@@ -0,0 +1,26 @@
+import "dart:ui" show Locale;
+
+import "package:sealed_countries/sealed_countries.dart" show NaturalLanguage;
+
+/// Extension on [NaturalLanguage] to convert it to a [Locale].
+extension NaturalLanguageExtension on NaturalLanguage {
+  /// Converts [NaturalLanguage] to a [Locale].
+  ///
+  /// The optional [countryCode] parameter specifies the country code for the
+  /// locale. The optional [scriptCode] parameter specifies the script code for
+  /// the locale. Method returns a [Locale] object representing the natural
+  /// language with the specified country and script codes.
+  ///
+  /// Example:
+  /// ```dart
+  /// const language = LangEng();
+  /// final locale = language.toLocale(countryCode: 'US');
+  /// print(locale); // Prints: en_US
+  /// ```
+  Locale toLocale({String? countryCode, String? scriptCode}) =>
+      Locale.fromSubtags(
+        languageCode: codeShort.toLowerCase(),
+        scriptCode: scriptCode,
+        countryCode: countryCode,
+      );
+}
---
+++ packages/world_countries/lib/src/extensions/world_country_extension.dart
@@ -0,0 +1,33 @@
+import "dart:ui" show Locale;
+
+import "package:sealed_countries/sealed_countries.dart";
+
+/// Extension on [WorldCountry] to convert it to a [Locale].
+extension WorldCountryExtension on WorldCountry {
+  /// Converts [WorldCountry] to a [Locale].
+  ///
+  /// The optional [languageCode] parameter specifies the language code for the
+  /// locale. If not provided, it defaults to "und" (undefined). The optional
+  /// [language] parameter specifies a [NaturalLanguage] object representing the
+  /// language of the locale - if provided, it is used instead of languageCode.
+  /// The optional [scriptCode] parameter specifies the script code for the
+  /// locale. Returns a [Locale] object representing the world country with the
+  /// specified language, script, and country codes.
+  ///
+  /// Example:
+  /// ```dart
+  /// final country = WorldCountry.unitedStates;
+  /// final locale = country.toLocale(languageCode: 'en');
+  /// print(locale); // Prints: en_US
+  /// ```
+  Locale toLocale({
+    String languageCode = "und",
+    NaturalLanguage? language,
+    String? scriptCode,
+  }) =>
+      Locale.fromSubtags(
+        languageCode: language?.codeShort.toLowerCase() ?? languageCode,
+        scriptCode: scriptCode,
+        countryCode: codeShort,
+      );
+}
