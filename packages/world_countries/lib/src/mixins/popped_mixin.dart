572d186e0b22edb7cbc9c13400e6bfc849464750
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2023-09-06 20:48:48 +0200

    doc: added dart doc to new mixins and extensions

--- packages/world_countries/lib/src/mixins/popped_mixin.dart
+++ packages/world_countries/lib/src/mixins/popped_mixin.dart
@@ -3,10 +3,17 @@ import "dart:async" show unawaited;
 import "package:flutter/foundation.dart";
 import "package:flutter/widgets.dart" show BuildContext;
 
-import "../extensions/build_context_extension.dart";
+import "../extensions/navigator_context_extension.dart";
 import "../widgets/base_widgets/stateful_indexed_list_view.dart";
 
+/// A mixin that provides a methods for popping the current route.
 mixin PoppedMixin<T extends Object> on StatefulIndexedListView<T> {
+  /// Selects the given [item] and pops the current route.
+  ///
+  /// This method calls the [onSelect] callback with the selected [item] and
+  /// then pops the current route using `maybePop()` from the [context].
+  ///
+  /// Returns the selected [item].
   @protected
   T maybeSelectAndPop(T item, BuildContext context) {
     onSelect?.call(item);
--- packages/world_countries/lib/src/mixins/properties_convertible_mixin.dart
+++ packages/world_countries/lib/src/mixins/properties_convertible_mixin.dart
@@ -4,11 +4,21 @@ import "package:flutter/widgets.dart" show BuildContext;
 import "../models/item_properties.dart";
 import "../widgets/base_widgets/stateful_indexed_list_view.dart";
 
+/// A mixin that provides methods for converting items into [ItemProperties].
 mixin PropertiesConvertibleMixin<T extends Object>
     on StatefulIndexedListView<T> {
+  /// Returns the [ItemProperties] for an item at the specified [index].
+  ///
+  /// The [context] parameter is the build context for the item.
   ItemProperties<T> properties(BuildContext context, int index) =>
       filteredProperties(items, context, index);
 
+  /// Creates [ItemProperties] for an item at the specified [index] using the
+  /// provided [filtered] items and [context].
+  ///
+  /// The [filtered] parameter is an iterable of items that have been filtered
+  /// based on the current configuration. The [context] parameter is the build
+  /// context for the item.
   @protected
   ItemProperties<T> filteredProperties(
     Iterable<T> filtered,
