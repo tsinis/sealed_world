2d3cdf976bd9d986d4da64e29fce26b6d82e190c
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2023-10-07 12:01:05 +0200

    feat(cli): added basic utils for cli tools

---
+++ tools/lib/common/dart_utils.dart
@@ -0,0 +1,26 @@
+import "dart:io";
+
+final class DartUtils {
+  const DartUtils();
+
+  Future<void> run(List<String> arguments) async {
+    final result = await Process.run("dart", arguments);
+    if (result.exitCode != 0) print(result.stderr); // ignore: avoid_print
+  }
+
+  Future<void> pub(List<String> arguments, {bool isGlobal = false}) => run([
+        "pub",
+        if (isGlobal) ...["global"] else ...[],
+        ...arguments,
+      ]);
+
+  Future<void> pubGet() => pub(["get"]);
+
+  Future<void> fixFormat() async {
+    await fixApply();
+    await format();
+  }
+
+  Future<void> fixApply() => run(["fix", "--apply"]);
+  Future<void> format() => run(["format", "."]);
+}
