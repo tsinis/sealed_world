6ed13315ecedeb75c54061b793b2fdcf59daba04
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2023-10-07 14:09:36 +0200

    docs(interface): addded dartdoc to named constructor

--- packages/sealed_countries/lib/src/interfaces/named.dart
+++ packages/sealed_countries/lib/src/interfaces/named.dart
@@ -1,7 +1,18 @@
 // TODO! Move to languages.
 /// An abstract interface class for objects that have a [String] name.
-abstract interface class Named {
-  const Named(this.name); // coverage:ignore-line
+///
+/// This interface provides a common structure for objects that have a name.
+/// It is intended to be implemented by classes that represent entities
+/// with a name.
+///
+/// Implementing classes should assign a value to the [name] field in their
+/// constructors. Once assigned, the [name] field cannot be modified.
+abstract class Named {
+  /// Constructs a new instance of the [Named] class with the given [name].
+  ///
+  /// The [name] parameter represents the name of the object.
+  /// It should be a non-null [String].
+  const Named(this.name);
 
   /// The common name of the object.
   final String name;
