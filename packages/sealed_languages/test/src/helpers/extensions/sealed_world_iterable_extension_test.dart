943ad42481a2f49683ce59d0c6cece0769b8733d
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2024-03-10 15:53:17 +0100

    test: added tests for script and iterable extension

--- packages/sealed_languages/test/src/helpers/extensions/sealed_world_iterable_extension_test.dart
+++ packages/sealed_languages/test/src/helpers/extensions/sealed_world_iterable_extension_test.dart
@@ -1,18 +1,39 @@
 import "package:sealed_languages/src/helpers/extensions/sealed_world_iterable_extension.dart";
+import "package:sealed_languages/src/model/script/writing_system.dart";
 import "package:test/test.dart";
 
-void main() => group("SealedWorldIterableExtension", () {
-      group("toInstancesString", () {
-        test("should return an empty string for an empty iterable", () {
-          const emptyList = <Object>[];
-          final result = emptyList.toInstancesString();
-          expect(result, "[]");
-        });
-
-        test("should return a string representation of the instances", () {
-          final list = <Object>[0, "1", 2.0];
-          final result = list.toInstancesString();
-          expect(result, equals("[$int(), $String(), $double()]"));
-        });
+void main() {
+  group(
+    "SealedWorldIterableExtension",
+    () => group("toInstancesString", () {
+      test("should return an empty string for an empty iterable", () {
+        const emptyList = <Object>[];
+        final result = emptyList.toInstancesString();
+        expect(result, "[]");
       });
-    });
+
+      test("should return a string representation of the instances", () {
+        final list = <Object>[0, "1", 2.0];
+        final result = list.toInstancesString();
+        expect(result, equals("[$int(), $String(), $double()]"));
+      });
+    }),
+  );
+
+  group(
+    "SealedWorldIterableIsoExtension",
+    () => group("firstIsoWhereOrNull", () {
+      List<Script>? noList;
+      test("with null iterable and assertNotEmpty: false", () {
+        final result =
+            noList.firstIsoWhereOrNull((_) => true, assertNotEmpty: false);
+        expect(result, isNull);
+      });
+
+      test("with null iterable and assertNotEmpty: true", () {
+        final result = noList.firstIsoWhereOrNull((_) => true);
+        expect(result, isNull);
+      });
+    }),
+  );
+}
