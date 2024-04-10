2e91dab29c9b87e9a2d377c17e41f11a1279b291
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2024-04-10 22:48:40 +0200

    refactor: iso translated iterable extension to use basic locale

--- packages/sealed_languages/lib/src/helpers/extensions/iso_translated_iterable_extension.dart
+++ packages/sealed_languages/lib/src/helpers/extensions/iso_translated_iterable_extension.dart
@@ -1,7 +1,5 @@
 import "../../interfaces/translated.dart";
 import "../../model/core/basic_locale.dart";
-import "../../model/language/language.dart";
-import "../../model/script/writing_system.dart";
 import "../../model/translated_name.dart";
 import "translated_extension.dart";
 
@@ -11,43 +9,39 @@ import "translated_extension.dart";
 /// This extension provides a method to generate a cache map of translations for
 /// a given locale. The cache map can be used to quickly lookup the
 /// translation for an [IsoTranslated] object.
-extension IsoTranslatedIterableExtension<T extends TranslatedName,
-    N extends Object, I extends IsoTranslated<T, N>> on Iterable<I> {
-  /// Generates a cache map of translations for the given locale.
+extension IsoTranslatedIterableExtension<
+    L extends BasicLocale,
+    T extends TranslatedName,
+    N extends Object,
+    I extends IsoTranslated<T, N>> on Iterable<I> {
+  /// Generates a cache map of translations for the given [locale].
   ///
   /// The cache map is a [Map] where the keys are [IsoTranslated] objects and
   /// the values are the corresponding translations in the given locale.
   ///
-  /// The [countryCode] and [script] parameters can be used to specify the
-  /// country and script for the translations. If [useLanguageFallback] is set
-  /// to `true`, the method will fall back to the language without filtering if
-  /// no matching translation is found.
+  /// The [BasicLocale] parameter with a required `language` parameter and
+  /// optional`countryCode` and `script` parameters can be used to specify the
+  /// locale for which to generate the translations. If [useLanguageFallback] is
+  ///  set to `true`, the method will fall back to the language without
+  /// filtering if no matching translation is found.
   ///
   /// The returned map is unmodifiable to prevent accidental modifications.
   ///
   /// Example usage:
   ///
   /// ```dart
-  /// final cache = NaturalLanguage.list.translationsCacheMap(
+  /// final cache = NaturalLanguage.list.translationsCacheMap(const BasicLocale(
   ///   LangEng(),
   ///   script: ScriptLatn(),
+  ///  ),
   /// );
   /// print(cache[const LangAfr()]?.name); // "Afrikaans"
   /// ```
-  Map<I, T> translationsCacheMap(
-    NaturalLanguage language, {
-    String? countryCode,
-    Script? script,
-    bool useLanguageFallback = true,
-  }) {
+  Map<I, T> translationsCacheMap(L locale, {bool useLanguageFallback = true}) {
     final map = <I, T>{};
     for (final item in this) {
       final translation = item.maybeTranslation(
-        BasicLocale(
-          language,
-          countryCode: countryCode,
-          script: script,
-        ),
+        locale,
         useLanguageFallback: useLanguageFallback,
       );
       if (translation != null) map[item] = translation;
--- packages/sealed_languages/test/src/helpers/extensions/iso_translated_iterable_extension_test.dart
+++ packages/sealed_languages/test/src/helpers/extensions/iso_translated_iterable_extension_test.dart
@@ -1,19 +1,32 @@
+import "package:_sealed_world_tests/sealed_world_tests.dart";
 import "package:sealed_languages/language_translations.dart";
+import "package:sealed_languages/src/data/natural_languages.data.dart";
 import "package:sealed_languages/src/helpers/extensions/iso_translated_iterable_extension.dart";
+import "package:sealed_languages/src/model/core/basic_locale.dart";
 import "package:sealed_languages/src/model/language/language.dart";
 import "package:test/test.dart";
 
 void main() => group("IsoTranslatedIterableExtension", () {
-      test(
-        """there should be a translations cache map for every language in supported languages""",
-        () {
-          for (final language in kSealedLanguagesSupportedLanguages) {
-            final cache = NaturalLanguage.list.translationsCacheMap(
-              language,
-              script: language.scripts.first,
-            );
-            expect(cache.length, NaturalLanguage.list.length);
-          }
-        },
-      );
+      group("translationsCacheMap", () {
+        performanceTest(
+          "languages in supported list should have all translations",
+          () {
+            for (final language in kSealedLanguagesSupportedLanguages) {
+              final cache = NaturalLanguage.list.translationsCacheMap(
+                BasicLocale(language, script: language.scripts.first),
+              );
+              expect(cache.length, NaturalLanguage.list.length);
+            }
+          },
+        );
+
+        performanceTest(
+          "some languages should have at least 1 translation, of it's own name",
+          () {
+            final cache = NaturalLanguage.list
+                .translationsCacheMap(const BasicLocale(LangKal()));
+            expect(cache.length, 1);
+          },
+        );
+      });
     });
