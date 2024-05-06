729d46d23e3c177a289f0c8000f3e88c04b442cc
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2024-05-06 22:25:32 +0200

    feat: created simple flags map (wip)

--- tools/bin/colors_extractor.dart
+++ tools/bin/colors_extractor.dart
@@ -2,30 +2,132 @@
 import "package:cli/utils/color_utils.dart";
 import "package:cli/utils/io_utils.dart";
 import "package:cli/utils/json_utils.dart";
+import "package:pure_dart_ui/pure_dart_ui.dart";
 import "package:sealed_countries/sealed_countries.dart";
 
 /// Usage: `dart run :colors_extractor`.
 Future<void> main() async {
   final io = IoUtils();
   var i = 0;
-  io.forFileInDirectory(
-    Directory(join(JsonUtils.defaultDataDirPath, "flags")),
-    format: ColorUtils.svgFormat,
-    withFile: (file, name) {
-      if (name.length > 2) return;
-      // ignore: avoid-substring, no emoji here.
-      final countryCode = name.substring(0, 2).toUpperCaseIsoCode();
-      final country = WorldCountry.maybeFromCodeShort(countryCode);
-      if (country == null) return;
-      final countryName = country.internationalName;
+  final buffer = StringBuffer("""
+import "colors_extractor.dart";
+import "package:pure_dart_ui/pure_dart_ui.dart";
+import "package:sealed_countries/sealed_countries.dart";
 
-      final svg = file.readAsStringSync();
-      final colors = ColorUtils([], countryName).extractColorsFromSvg(svg);
-      if (colors.isEmpty) return print("No colors for $countryName!");
-      print("$countryName country flag has ${colors.length} colors");
-      i++;
-    },
-  );
+const map = {
+  """);
+  io
+    ..forFileInDirectory(
+      Directory(join(JsonUtils.defaultDataDirPath, "flags")),
+      format: ColorUtils.svgFormat,
+      withFile: (file, name) {
+        if (name.length > 2) return;
+        // ignore: avoid-substring, no emoji here.
+        final countryCode = name.substring(0, 2).toUpperCaseIsoCode();
+        final country = WorldCountry.maybeFromCodeShort(countryCode);
+        if (country == null) return;
+        final countryName = country.internationalName;
+
+        final svg = file.readAsStringSync();
+        final colors = ColorUtils([], countryName).extractColorsFromSvg(svg);
+        if (colors.isEmpty) return print("No colors for $countryName!");
+        final properties =
+            FlagProperties(colors.map(ColorsProperties.new).toList());
+        buffer
+          ..write("\n/// ${country.internationalName}: ${country.codeShort}.\n")
+          ..write("${country.runtimeType}(): $properties,");
+        i++;
+      },
+    )
+    ..writeContentToFile("flags_map.txt", "$buffer};");
 
   print("Conversion completed successfully. $i files converted.");
+}
+
+class FlagProperties {
+  const FlagProperties(
+    this.colors, {
+    this.backgroundColor,
+    this.aspectRatio = 2 / 3,
+    this.isHorizontalStriped = true,
+    this.symbolsProperties,
+    this.url,
+  });
+
+  final Color? backgroundColor;
+  final List<ColorsProperties> colors;
+  final List<SymbolsProperties>? symbolsProperties;
+  final double aspectRatio;
+  final String? url;
+
+  /// Indicates whether the stripes on the flag are arranged horizontally.
+  ///
+  /// * If the value is `false`, the stripes run from left to right, i.e. Italy,
+  /// France, Ireland, etc., like flags.
+  /// * If the value is `true`, the stripes run from the bottom to the top, i.e.
+  /// Russia, Germany, Poland, Latvia, etc., like flags.
+  /// * If the value is `null`, it means that it is not clear whether the
+  /// stripes are arranged horizontally or vertically, or the flag may not
+  /// have any stripes at all.
+  final bool? isHorizontalStriped;
+
+  @override
+  String toString() => "FlagProperties($colors, "
+      "backgroundColor: $backgroundColor, aspectRatio: $aspectRatio, "
+      "isHorizontalStriped: $isHorizontalStriped, "
+      "symbolsProperties: $symbolsProperties)";
+}
+
+class ColorsProperties {
+  const ColorsProperties(this.color, {this.ratio = 1})
+      : assert(ratio >= 0, "Ratio must be greater than or equal to zero.");
+
+  final int ratio;
+  final Color color;
+
+  @override
+  String toString() => "ColorsProperties($color, ratio: $ratio)";
+}
+
+class SymbolsProperties {
+  /// Creates an alignment.
+  const SymbolsProperties(
+    this.colors, {
+    this.x = middle,
+    this.y = middle,
+    this.heightFactor,
+    this.widthFactor,
+    this.rotation = 0,
+  })  : assert(widthFactor == null || widthFactor >= 0.0),
+        assert(heightFactor == null || heightFactor >= 0.0);
+
+  static const middle = 0.5;
+
+  final double? widthFactor;
+  final double? heightFactor;
+  final List<Color> colors;
+  final double rotation;
+
+  /// The distance fraction in the horizontal direction.
+  ///
+  /// A value of -1.0 corresponds to the leftmost edge. A value of 1.0
+  /// corresponds to the rightmost edge. Values are not limited to that range;
+  /// values less than -1.0 represent positions to the left of the left edge,
+  /// and values greater than 1.0 represent positions to the right of the right
+  /// edge.
+  final double x;
+
+  /// The distance fraction in the vertical direction.
+  ///
+  /// A value of -1.0 corresponds to the topmost edge. A value of 1.0
+  /// corresponds to the bottommost edge. Values are not limited to that range;
+  /// values less than -1.0 represent positions above the top, and values
+  /// greater than 1.0 represent positions below the bottom.
+  final double y;
+
+  @override
+  String toString() =>
+      "SymbolsProperties(${x.toStringAsFixed(1)}, ${y.toStringAsFixed(1)}"
+      "widthFactor: $widthFactor, heightFactor: $heightFactor, "
+      "colors: $colors, rotation: $rotation)";
 }
