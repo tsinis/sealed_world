faf2697538f2e8479c158ba7ef3adcaf0ea263ce
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2024-05-27 09:11:25 +0200

    feat: added properties map in collections

--- packages/flags/lib/flags.dart
+++ packages/flags/lib/flags.dart
@@ -4,5 +4,6 @@ library flags;
 export "package:sealed_countries/country_translations.dart";
 export "package:sealed_countries/sealed_countries.dart";
 
+export "src/collections/properties_map.dart";
 export "src/model/colors_properties.dart";
 export "src/model/flag_properties.dart";
