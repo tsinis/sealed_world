80e44cf9f851b534b9a04b8ee0b9a50a817c9ae8
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2024-05-27 23:00:47 +0200

    feat(docs): added original flags png overlay in the example app

--- packages/flags/example/lib/main.dart
+++ packages/flags/example/lib/main.dart
@@ -4,22 +4,18 @@ import "package:flutter/material.dart";
 import "ui/flags/simple_stripes_flag.dart";
 
 void main() => runApp(
-      MaterialApp(
-        home: const Main(title: "Flags Demo Home Page"),
-        theme: ThemeData.dark(useMaterial3: true),
-      ),
+      MaterialApp(home: const Main(), theme: ThemeData.dark()),
     );
 
 class Main extends StatefulWidget {
-  const Main({required this.title, super.key});
-
-  final String title;
+  const Main({super.key});
 
   @override
   State<Main> createState() => _MainState();
 }
 
 class _MainState extends State<Main> {
+  double opacity = 0;
   int index = 0;
 
   void _incrementIndex() => setState(() => index++);
@@ -28,15 +24,42 @@ class _MainState extends State<Main> {
   Widget build(BuildContext context) {
     final flagData = flagPropertiesMap.entries.elementAt(index);
 
-    return Scaffold(
-      appBar: AppBar(title: SelectableText(flagData.key.internationalName)),
-      body: Center(
-        child: SizedBox(
-          width: 200,
-          child: GestureDetector(
-            onTap: _incrementIndex,
-            child: SimpleStripesFlag(flagData.value),
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(48),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Slider(
+              value: opacity,
+              onChanged: (newOpacity) => setState(() => opacity = newOpacity),
+            ),
+            backgroundColor: Colors.transparent,
           ),
+          body: Center(
+            child: Stack(
+              alignment: Alignment.center,
+              clipBehavior: Clip.none,
+              children: [
+                Image.network(flagData.key.flagPngUrl(), scale: 0.1),
+                Opacity(
+                  opacity: opacity,
+                  child: GestureDetector(
+                    onTap: _incrementIndex,
+                    child: SimpleStripesFlag(
+                      flagData.value,
+                      borderRadius: Radius.zero,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          bottomNavigationBar: SelectableText(
+            flagData.key.internationalName,
+            style: const TextStyle(color: Colors.grey, fontSize: 32),
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.transparent,
         ),
       ),
     );
