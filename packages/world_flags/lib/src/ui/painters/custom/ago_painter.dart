5cdcfabc9c372f3a901b417a4286eae88e44b22f
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2024-07-28 16:15:48 +0200

    refactor(ui): refactor ago painter

--- packages/world_flags/lib/src/ui/painters/custom/ago_painter.dart
+++ packages/world_flags/lib/src/ui/painters/custom/ago_painter.dart
@@ -20,10 +20,20 @@ final class AgoPainter extends CustomElementsPainter {
     Size size, [
     FlagParentBounds? parent,
   ]) {
-    final adjustedSize = ratioAdjustedSize(size, minRatio: 2);
-    final center = calculateCenter(size);
+    final adjustedSize = ratioAdjustedSize(size, minRatio: 1.4);
     final height = adjustedSize.height * 2;
     final width = adjustedSize.width;
+
+    final starProperties = ElementsProperties(
+      property.mainColor,
+      shape: const Star(),
+      offset: const Offset(-0.025, -0.25),
+      widthFactor: 0.11,
+    );
+
+    final star = StarPainter([starProperties], aspectRatio)
+        .paintFlagElements(canvas, size);
+    final center = star.bounds.center;
 
     final path = Path()
       ..moveTo(width * 0.61, height * 1.33)
@@ -267,20 +277,11 @@ final class AgoPainter extends CustomElementsPainter {
     final bounds = path.getBounds();
 
     canvas
-      ..save()
-      ..translate(center.dx - bounds.width, center.dy - bounds.height)
+      ..translate(
+          center.dx - bounds.width * 1.2, center.dy - bounds.height * 0.78)
       ..drawPath(path, paintCreator())
-      ..drawPath(path2, paintCreator())
-      ..restore();
+      ..drawPath(path2, paintCreator());
 
-    final starProperties = ElementsProperties(
-      property.mainColor,
-      shape: const Star(),
-      offset: const Offset(-0.025, -0.25),
-      widthFactor: 0.11,
-    );
-
-    return StarPainter([starProperties], aspectRatio)
-        .paintFlagElements(canvas, size);
+    return star;
   }
 }
