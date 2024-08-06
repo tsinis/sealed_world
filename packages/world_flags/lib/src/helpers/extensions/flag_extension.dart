0f9ee450cc17cbb1ee3bfcdc46ebf69c2b8b4357
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2024-08-06 22:18:41 +0200

    docs: provide documentation for v1.1.0

--- packages/world_flags/lib/src/helpers/extensions/flag_extension.dart
+++ packages/world_flags/lib/src/helpers/extensions/flag_extension.dart
@@ -1,4 +1,4 @@
-import "dart:ui";
+import "dart:ui" show Color;
 
 import "package:sealed_countries/sealed_countries.dart";
 
@@ -7,16 +7,23 @@ import "../../model/elements/custom_elements_properties.dart";
 import "../../model/elements/elements_properties.dart";
 import "../../model/flag_properties.dart";
 
+/// Extension on [WorldCountry] to provide flag-related properties and colors.
 extension FlagExtension on WorldCountry {
+  /// Retrieves the [FlagProperties] associated with the default [WorldCountry].
   FlagProperties? get flagProperties => flagPropertiesMap[this];
 
+  /// Combines [flagStripeColors] and [flagElementsColors] into a single list of
+  /// colors.
   List<Color> get flagColors =>
       List.unmodifiable([...flagStripeColors, ...flagElementsColors]);
 
+  /// Retrieves the stripe colors of the flag from [FlagProperties].
   List<Color> get flagStripeColors => List.unmodifiable(
         flagProperties?.stripeColors.map((stripe) => stripe.color) ?? const [],
       );
 
+  /// Retrieves the element colors of the flag from [FlagProperties]. If the
+  /// elements have custom properties, their additional colors are included.
   List<Color> get flagElementsColors {
     final colors = <Color>[];
     final properties = flagProperties?.elementsProperties;
