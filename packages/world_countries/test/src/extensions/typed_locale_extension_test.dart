e80764bf6b5e60b2fb32d67300d28fe906d02f6c
codefactor-io <support@codefactor.io>
2025-01-25 12:16:59 +0000

    [CodeFactor] Apply fixes

--- packages/world_countries/test/src/extensions/typed_locale_extension_test.dart
+++ packages/world_countries/test/src/extensions/typed_locale_extension_test.dart
@@ -12,7 +12,7 @@ void main() => group("TypedLocaleExtension", () {
 
       group("getters", () {
         test("asBasicLocale returns correct $BasicLocale instance", () {
-          final basicLocale = locale.asBasicLocale;
+          const basicLocale = locale.asBasicLocale;
 
           expect(basicLocale, isA<BasicLocale>());
           expect(basicLocale.language, equals(locale.language));
@@ -23,7 +23,7 @@ void main() => group("TypedLocaleExtension", () {
         test(
           "asBasicTypedLocale returns correct $BasicTypedLocale instance",
           () {
-            final basicTypedLocale = locale.asBasicTypedLocale;
+            const basicTypedLocale = locale.asBasicTypedLocale;
 
             expect(basicTypedLocale, isA<BasicTypedLocale>());
             expect(basicTypedLocale.language, equals(locale.language));
@@ -34,7 +34,7 @@ void main() => group("TypedLocaleExtension", () {
         );
 
         test("asLocale returns correct SDK Locale instance", () {
-          final sdkLocale = locale.asLocale;
+          const sdkLocale = locale.asLocale;
 
           expect(sdkLocale, isA<Locale>());
           expect(sdkLocale.languageCode, equals(locale.languageCode));
