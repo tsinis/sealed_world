df39abce6dd4e825ab982e8a897badd0b66cbe3d
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-03-22 12:09:24 +0100

    chore(example): refactor project runners and update configs for world_countries example

---
+++ packages/world_countries/example/android/app/src/main/kotlin/world/countries/world_countries_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package world.countries.world_countries_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- packages/world_countries/example/lib/assets/assets.gen.dart
+++ packages/world_countries/example/lib/assets/assets.gen.dart
@@ -45,11 +45,7 @@ class Assets {
 }
 
 class AssetGenImage {
-  const AssetGenImage(
-    this._assetName, {
-    this.size,
-    this.flavors = const {},
-  });
+  const AssetGenImage(this._assetName, {this.size, this.flavors = const {}});
 
   final String _assetName;
 
@@ -109,15 +105,8 @@ class AssetGenImage {
     );
   }
 
-  ImageProvider provider({
-    AssetBundle? bundle,
-    String? package,
-  }) {
-    return AssetImage(
-      _assetName,
-      bundle: bundle,
-      package: package,
-    );
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
   }
 
   String get path => _assetName;
