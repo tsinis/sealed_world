52402219d775dcb4e3048d715fd2674662cab93f
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-03-29 21:14:34 +0100

    feat(ui): extract items in country, currency, language pickers to extensions

---
+++ packages/world_countries/lib/src/extensions/pickers/currency_picker_extension.dart
@@ -0,0 +1,9 @@
+import "package:world_flags/world_flags.dart" show FiatCurrency;
+
+import "../../widgets/currency/currency_picker.dart";
+
+/// Extension on [CurrencyPicker] to provide additional functionality.
+extension CurrencyPickerExtension on CurrencyPicker {
+  /// Returns the list of fiat currencies being displayed by this widget.
+  Iterable<FiatCurrency> get currencies => items;
+}
