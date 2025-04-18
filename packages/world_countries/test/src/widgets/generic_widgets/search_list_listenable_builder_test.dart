f75901ff8d22a5b2214b4dc3c8b6e6d924190de3
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-04-18 21:06:56 +0200

    test(ui): add widget tests for search list listenable builder

---
+++ packages/world_countries/test/src/widgets/generic_widgets/search_list_listenable_builder_test.dart
@@ -0,0 +1,58 @@
+import "package:flutter/material.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:world_countries/src/widgets/generic_widgets/search_list_listenable_builder.dart";
+
+import "../../../helpers/widget_tester_extension.dart";
+
+void main() => group("$SearchListListenableBuilder", () {
+  const items = ["Apple", "Banana", "Cherry"];
+
+  testWidgets("initializes with correct items", (tester) async {
+    final controller = TextEditingController();
+    final lastBuiltList = <String>[];
+
+    await tester.pumpWidgetsApp(
+      SearchListListenableBuilder(
+        items: items,
+        searchIn: (item, _) => [item],
+        textController: controller,
+        builder: (_, list) {
+          lastBuiltList.addAll(list);
+
+          return const SizedBox.shrink();
+        },
+      ),
+    );
+
+    expect(lastBuiltList, items);
+    controller.dispose();
+  });
+
+  testWidgets("triggers didUpdateWidget on controller change", (tester) async {
+    final anotherController = TextEditingController();
+    final controller = TextEditingController();
+    final lastBuiltList = <String>[];
+
+    await tester.pumpWidgetsApp(
+      SearchListListenableBuilder<String>(
+        items: items,
+        searchIn: (item, _) => [item],
+        textController: controller,
+        builder: (_, _) => const SizedBox.shrink(),
+      ),
+    );
+
+    await tester.pumpWidgetsApp(
+      SearchListListenableBuilder<String>(
+        items: items,
+        searchIn: (item, _) => [item],
+        textController: anotherController,
+        builder: (_, _) => const SizedBox.shrink(),
+      ),
+    );
+
+    expect(lastBuiltList, isEmpty);
+    anotherController.dispose();
+    controller.dispose();
+  });
+});
