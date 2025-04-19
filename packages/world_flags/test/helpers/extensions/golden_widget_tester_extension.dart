12f6a696cba6b01cf5191d8c7db1c23cd48ff3f0
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-04-19 22:35:33 +0200

    refactor(deps): remove arabic_font dev dependency

--- packages/world_flags/test/helpers/extensions/golden_widget_tester_extension.dart
+++ packages/world_flags/test/helpers/extensions/golden_widget_tester_extension.dart
@@ -3,7 +3,6 @@ import "dart:io"; // Due to platform differences in golden tests.
 
 import "package:flutter/material.dart";
 import "package:flutter_test/flutter_test.dart";
-import "package:golden_toolkit/golden_toolkit.dart";
 import "package:world_flags/world_flags.dart";
 
 import "../flag_type.dart";
@@ -17,10 +16,6 @@ extension GoldenWidgetTesterExtension on WidgetTester {
     final aspectRatio = country.flagProperties?.aspectRatio ?? 1;
     final width = type == FlagType.decorated ? height : height * aspectRatio;
     final file = "../../goldens/${type.name}/${country.code.toLowerCase()}.png";
-
-    if (country.isAfg || country.isSau || country.isIrq || country.isVir) {
-      await loadAppFonts();
-    }
 
     await binding.setSurfaceSize(Size(width, height));
     await pumpWidget(
