99c64725177f4380af8f3c92704fb26ffb28dcf3
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-05-21 00:14:29 +0200

    test: add unit tests for irq painter functionality

---
+++ packages/world_flags/test/src/ui/painters/custom/irq_painter_test.dart
@@ -0,0 +1,44 @@
+import "package:flutter/rendering.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:world_flags/world_flags.dart";
+
+void main() => group("$IrqPainter", () {
+  final painter = IrqPainter(
+    flagIrqProperties.elementsProperties,
+    flagIrqProperties.aspectRatio,
+  );
+
+  test("ratio > originalAspectRatio (flag is wider)", () {
+    const size = Size(300, 100);
+    final canvas = _IrqPainterTest(size);
+
+    painter.paintFlagElements(canvas, size);
+    expect(canvas.saveCount, greaterThan(0));
+    expect(canvas.restoreCount, greaterThan(0));
+  });
+
+  test("ratio < originalAspectRatio (flag is taller)", () {
+    const size = Size(100, 300);
+    final canvas = _IrqPainterTest(size);
+    painter.paintFlagElements(canvas, size);
+    expect(canvas.saveCount, greaterThan(0));
+    expect(canvas.restoreCount, greaterThan(0));
+  });
+});
+
+class _IrqPainterTest implements Canvas {
+  _IrqPainterTest(this.size);
+
+  final Size size;
+  int restoreCount = 0; // ignore: omit_obvious_property_types, could be double.
+  int saveCount = 0; // ignore: omit_obvious_property_types, could be double.
+
+  @override
+  void save() => saveCount += 1;
+
+  @override
+  void restore() => restoreCount += 1;
+
+  @override
+  void noSuchMethod(Invocation invocation) => invocation;
+}
