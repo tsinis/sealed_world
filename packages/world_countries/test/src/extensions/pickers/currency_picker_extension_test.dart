fbf338a6429de795d7ffdc1a6271991ab1fcfc81
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-05-21 07:53:00 +0200

    chore: update `world_flags` dependency to v2.4.0

--- packages/world_countries/test/src/extensions/pickers/currency_picker_extension_test.dart
+++ packages/world_countries/test/src/extensions/pickers/currency_picker_extension_test.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unnecessary_async, just a test.
-
 import "package:flutter_test/flutter_test.dart";
 import "package:world_countries/src/extensions/pickers/currency_picker_extension.dart";
 import "package:world_countries/src/widgets/currency/currency_picker.dart";
--- packages/world_countries/test/src/extensions/pickers/language_picker_extension_test.dart
+++ packages/world_countries/test/src/extensions/pickers/language_picker_extension_test.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unnecessary_async, just a test.
-
 import "package:flutter_test/flutter_test.dart";
 import "package:world_countries/src/extensions/pickers/language_picker_extension.dart";
 import "package:world_countries/src/widgets/language/language_picker.dart";
