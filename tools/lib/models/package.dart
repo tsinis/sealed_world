a28af738d4819f186ae797e2a3f66a47d15d6c07
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-07-03 22:16:26 +0200

    refactor(cli): remove unused `select_annotation` package

--- tools/lib/models/package.dart
+++ tools/lib/models/package.dart
@@ -1,11 +1,9 @@
+// ignore_for_file: avoid-unnecessary-enum-prefix, avoid-shadowing
+
 import "package:change_case/change_case.dart";
-import "package:select_annotation/select_annotation.dart";
 
 import "../constants/path_constants.dart";
 
-part "package.select.dart";
-
-@matchable
 enum Package {
   sealedLanguages,
   sealedCurrencies,
@@ -15,4 +13,44 @@ enum Package {
 
   String get fullPath =>
       "${PathConstants.packages}/$dirName/${PathConstants.lib}/${PathConstants.src}/";
+
+  T when<T>({
+    required T Function() sealedLanguages,
+    required T Function() sealedCurrencies,
+    required T Function() sealedCountries,
+  }) => switch (this) {
+    Package.sealedLanguages => sealedLanguages(),
+    Package.sealedCurrencies => sealedCurrencies(),
+    Package.sealedCountries => sealedCountries(),
+  };
+
+  T whenConst<T>({
+    required T sealedLanguages,
+    required T sealedCurrencies,
+    required T sealedCountries,
+  }) => switch (this) {
+    Package.sealedLanguages => sealedLanguages,
+    Package.sealedCurrencies => sealedCurrencies,
+    Package.sealedCountries => sealedCountries,
+  };
+
+  T? whenOrNull<T>({
+    T Function()? sealedLanguages,
+    T Function()? sealedCurrencies,
+    T Function()? sealedCountries,
+  }) => switch (this) {
+    Package.sealedLanguages => sealedLanguages?.call(),
+    Package.sealedCurrencies => sealedCurrencies?.call(),
+    Package.sealedCountries => sealedCountries?.call(),
+  };
+
+  T? whenConstOrNull<T>({
+    T? sealedLanguages,
+    T? sealedCurrencies,
+    T? sealedCountries,
+  }) => switch (this) {
+    Package.sealedLanguages => sealedLanguages,
+    Package.sealedCurrencies => sealedCurrencies,
+    Package.sealedCountries => sealedCountries,
+  };
 }
