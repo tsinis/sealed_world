23bf6c6d2ab76ada27cc91571e20904b5e5a7918
tsinis <52065414+tsinis@users.noreply.github.com>
2025-09-07 07:40:59 +0000

    Update gh-pages to output generated at e7280c1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"56f64c26474b70c7bc4d4c8785097499775d59
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2554869069"
+    serviceWorkerVersion: "1614942872"
   }
 });
