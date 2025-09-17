e830c18d2d24a08f31ce3d62abd91119fe2ea0c2
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-09-17 21:35:52 +0200

    feat(helpers): add maybe num extension for nullable `num` utilities
    This extension provides allocation-free helpers for nullable num types,
    improving readability in conditions without requiring unwrapping.

--- packages/world_countries/lib/helpers.dart
+++ packages/world_countries/lib/helpers.dart
@@ -4,6 +4,7 @@ library helpers;
 export "src/constants/ui_constants.dart";
 export "src/extensions/build_context_extension.dart";
 export "src/extensions/core/duration_extension.dart";
+export "src/extensions/core/maybe_num_extension.dart";
 export "src/extensions/world_countries_build_context_extension.dart";
 export "src/helpers/functional_platform.dart";
 export "src/helpers/name_text_input.dart";
