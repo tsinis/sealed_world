6a30a5a759eedf65e7b0976a61f063ccca3e8ddd
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-09-29 22:21:53 +0200

    refactor(helper): remove redundant asserts in copy-with methods

--- packages/sealed_currencies/lib/src/helpers/fiat_currency/fiat_currency_copy_with.dart
+++ packages/sealed_currencies/lib/src/helpers/fiat_currency/fiat_currency_copy_with.dart
@@ -35,63 +35,22 @@ extension FiatCurrencyCopyWith<T extends FiatCurrency> on T {
     String? thousandsSeparator,
     List<TranslatedName>? translations,
     bool? unitFirst,
-  }) {
-    final newCode = code ?? this.code;
-    final newCodeNumeric = codeNumeric ?? this.codeNumeric;
-    final newAltSymbols = alternateSymbols ?? this.alternateSymbols;
-    final newDenomination = smallestDenomination ?? this.smallestDenomination;
-    final newHtmlEntity = htmlEntity ?? this.htmlEntity;
-    final newNamesNative = namesNative ?? this.namesNative;
-    final newSubunit = subunit ?? this.subunit;
-    final newSymbol = symbol ?? this.symbol;
-
-    assert(
-      newSymbol == null || newSymbol.length > 0,
-      "`symbol` should not be empty!",
-    );
-    assert(newNamesNative.isNotEmpty, "`namesNative` should not be empty!");
-    assert(
-      newCode.length == IsoStandardized.codeLength,
-      "`code` should be exactly ${IsoStandardized.codeLength} characters long!",
-    );
-    assert(
-      newCodeNumeric.length == IsoStandardized.codeLength,
-      """`codeNumeric` should be exactly ${IsoStandardized.codeLength} characters long!""",
-    );
-    assert(
-      newHtmlEntity == null || newHtmlEntity.isNotEmpty,
-      "`htmlEntity` should not be empty!",
-    );
-    assert(
-      newSubunit == null || newSubunit.isNotEmpty,
-      "`subunit` should not be empty!",
-    );
-    assert(
-      newAltSymbols == null || newAltSymbols.isNotEmpty,
-      "`alternateSymbols` should not be empty!",
-    );
-    assert(
-      !newDenomination.isNegative,
-      "`smallestDenomination` should not be negative!",
-    );
-
-    return FiatCurrency(
-      code: code ?? this.code,
-      name: name ?? this.name,
-      namesNative: namesNative ?? this.namesNative,
-      codeNumeric: codeNumeric ?? this.codeNumeric,
-      translations: translations ?? this.translations,
-      alternateSymbols: alternateSymbols ?? this.alternateSymbols,
-      disambiguateSymbol: disambiguateSymbol ?? this.disambiguateSymbol,
-      htmlEntity: htmlEntity ?? this.htmlEntity,
-      priority: priority ?? this.priority,
-      smallestDenomination: smallestDenomination ?? this.smallestDenomination,
-      subunit: subunit ?? this.subunit,
-      subunitToUnit: subunitToUnit ?? this.subunitToUnit,
-      unitFirst: unitFirst ?? this.unitFirst,
-      symbol: symbol ?? this.symbol,
-      decimalMark: decimalMark ?? this.decimalMark,
-      thousandsSeparator: thousandsSeparator ?? this.thousandsSeparator,
-    );
-  }
+  }) => FiatCurrency(
+    code: code ?? this.code,
+    name: name ?? this.name,
+    namesNative: namesNative ?? this.namesNative,
+    codeNumeric: codeNumeric ?? this.codeNumeric,
+    translations: translations ?? this.translations,
+    alternateSymbols: alternateSymbols ?? this.alternateSymbols,
+    disambiguateSymbol: disambiguateSymbol ?? this.disambiguateSymbol,
+    htmlEntity: htmlEntity ?? this.htmlEntity,
+    priority: priority ?? this.priority,
+    smallestDenomination: smallestDenomination ?? this.smallestDenomination,
+    subunit: subunit ?? this.subunit,
+    subunitToUnit: subunitToUnit ?? this.subunitToUnit,
+    unitFirst: unitFirst ?? this.unitFirst,
+    symbol: symbol ?? this.symbol,
+    decimalMark: decimalMark ?? this.decimalMark,
+    thousandsSeparator: thousandsSeparator ?? this.thousandsSeparator,
+  );
 }
