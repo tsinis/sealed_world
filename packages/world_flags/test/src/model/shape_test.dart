7972b1068d2b2d04e8e695c5b901dad117453db4
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-10-10 21:13:28 +0200

    test: add unit tests for shape constructors
    Added tests for Triangle, Rectangle, Star, Ellipse, Moon, and DiagonalLine
    constructors to ensure correct instantiation and properties.

--- packages/world_flags/test/src/model/shape_test.dart
+++ packages/world_flags/test/src/model/shape_test.dart
@@ -3,6 +3,38 @@ import "package:flutter_test/flutter_test.dart";
 import "package:world_flags/src/model/shape.dart";
 
 void main() => group("$Shape", () {
+  group("constructors", () {
+    test(
+      "creates $Triangle with no parameters",
+      () => expect(const Triangle(), isA<Triangle>()),
+    );
+
+    test("creates $Rectangle with 2.0 aspect ratio", () {
+      const instance = Rectangle(aspectRatio: 2);
+      expect(instance.aspectRatio, 2);
+    });
+
+    test("creates $Star with 2 points", () {
+      const instance = Star(points: 2);
+      expect(instance.points, 2);
+    });
+
+    test(
+      "creates $Ellipse with no parameters",
+      () => expect(const Ellipse(), isA<Ellipse>()),
+    );
+
+    test("creates $Moon with 2.0 radius", () {
+      const instance = Moon(radius: 2);
+      expect(instance.radius, 2);
+    });
+
+    test("creates bottom left to top right $DiagonalLine", () {
+      const instance = DiagonalLine(isTopRightToBottomLeft: false);
+      expect(instance.isTopRightToBottomLeft, isFalse);
+    });
+  });
+
   group("asserts", () {
     const negative = -1.0;
 
