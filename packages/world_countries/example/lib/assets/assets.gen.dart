c4a0ef0137c408a2aa8b2f53d4e05513d1f69390
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2026-08-02 09:30:15 +0200

    chore(deps): update `world_countires` to v4.4.0

--- packages/world_countries/example/lib/assets/assets.gen.dart
+++ packages/world_countries/example/lib/assets/assets.gen.dart
@@ -39,9 +39,7 @@ class $WebIconsGen {
   List<AssetGenImage> get values => [icon192];
 }
 
-class Assets {
-  const Assets._();
-
+abstract final class Assets {
   static const $AssetsImagesGen images = $AssetsImagesGen();
   static const $WebGen web = $WebGen();
 }
